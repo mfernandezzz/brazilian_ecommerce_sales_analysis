8d75006b6893b4a79aafdcdd57adfa8ea0bf4733
mfernandez <137459046+mfernandezzz@users.noreply.github.com>
2026-09-19 20:02:45 -0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,4 +1,3 @@
--- 1) Top products
 -- Top 10 products with highest quantity of orders
 SELECT product_category_name AS product, COUNT(order_items.order_id) AS orders
 FROM products
@@ -22,14 +21,14 @@ FROM payments
 GROUP BY payment_type
 ORDER BY orders DESC;
 
--- Payments type, total_orders and the revenue by payment_type
+-- Payments type, total_orders and revenue
 SELECT payments.payment_type AS payment_type, COUNT(orders.order_id) AS orders, SUM(payments.payment_value) AS revenue
 FROM payments
 INNER JOIN orders ON payments.order_id = orders.order_id
 GROUP BY payment_type
 ORDER BY revenue DESC;
 
--- Select all the data for those orders with a not_defined payment_type
+-- All the data for those orders with a not_defined payment_type
 SELECT * FROM orders
 INNER JOIN payments ON orders.order_id = payments.order_id
 WHERE payment_type = 'not_defined';
@@ -51,7 +50,7 @@ GROUP BY city, state
 ORDER BY customers DESC
 LIMIT 10;
 
--- SELECT the products and the respective amount of orders for those purchases made with a debit card (debit_card payment type)
+-- The products and the respective amount of orders for those purchases made with a debit card (debit_card payment type)
 SELECT product_category_name, COUNT(orders.order_id) AS orders
 FROM products
 INNER JOIN order_items ON products.product_id = order_items.product_id
@@ -61,10 +60,10 @@ WHERE payment_type = 'debit_card'
 GROUP BY product_category_name
 ORDER BY orders DESC;
 
--- 2) RFM Segmentation: RFM segmentation is a marketing method for grouping customers based on:
+-- RFM Segmentation: RFM segmentation is a marketing method for grouping customers based on:
 -- Recency: how recently they purchased
 -- Frequency: how often they purchase
--- Moentary Value: how much they spend
+-- Monetary Value: how much they spend
 SELECT customer.customer_id AS id, customer.customer_city AS city, customer_state AS state, 
     MAX(orders.order_purchase_timestamp) AS last_purchase, 
     COUNT(orders.order_id) AS orders, 
@@ -76,7 +75,7 @@ GROUP BY id, city, state
 ORDER BY total_spend DESC
 LIMIT 10;
 
--- Select the City, customers per city, total spending by customers in each city, total orders, and last purchase date
+-- The City, customers per city, total spending by customers in each city, total orders, and last purchase date
 SELECT customer.customer_city AS city, COUNT(customer.customer_id) AS customers, 
     SUM(payments.payment_value) AS total_spending,
     COUNT(orders.order_id) AS total_orders,
@@ -88,7 +87,19 @@ GROUP BY city
 ORDER BY total_spending DESC
 LIMIT 10;
 
--- 3) yearly_revenue_trend and monthly_revenue_trend
+-- Identify the top 10 sellers, their city, state, last sell date, last_sell made, quantity of orders and total revenue
+SELECT sellers.seller_id AS id, sellers.seller_city AS city, sellers.seller_state AS state, 
+    MAX(orders.order_purchase_timestamp) AS last_sell,
+    COUNT(orders.order_id) AS orders, 
+    SUM(payments.payment_value) AS revenue
+FROM sellers
+INNER JOIN order_items ON sellers.seller_id = order_items.seller_id
+INNER JOIN orders ON order_items.order_id = orders.order_id
+INNER JOIN payments ON orders.order_id = payments.order_id
+GROUP BY id, city, state 
+ORDER BY revenue DESC
+LIMIT 10;
+
 -- Get the total revenue by year
 SELECT DISTINCT (EXTRACT(year FROM order_purchase_timestamp)) AS year, SUM(payments.payment_value) AS revenue
 FROM orders
@@ -104,3 +115,26 @@ FROM orders
 INNER JOIN payments ON orders.order_id = payments.order_id
 GROUP BY year, month
 ORDER BY year ASC;
+
+-- Show the products, the quantity of orders and the revenue for each one during the year 2017
+SELECT products.product_category_name AS product, COUNT(orders.order_id) AS orders, SUM(payments.payment_value) AS revenue
+FROM products
+INNER JOIN order_items ON products.product_id = order_items.product_id
+INNER JOIN orders ON order_items.order_id = orders.order_id
+INNER JOIN payments ON orders.order_id = payments.order_id
+WHERE EXTRACT(year FROM orders.order_purchase_timestamp) = 2017
+GROUP BY product
+ORDER BY orders DESC
+LIMIT 10;
+
+-- Total orders and revenue per product with a freight_value above the average
+SELECT products.product_category_name AS product,
+    COUNT(orders.order_id) AS orders, 
+    SUM(payments.payment_value) AS revenue
+FROM products
+INNER JOIN order_items ON products.product_id = order_items.product_id
+INNER JOIN orders ON order_items.order_id = orders.order_id
+INNER JOIN payments ON orders.order_id = payments.order_id
+WHERE freight_value > (SELECT AVG(freight_value) FROM order_items)
+GROUP BY product, freight_value
+ORDER BY revenue DESC;
